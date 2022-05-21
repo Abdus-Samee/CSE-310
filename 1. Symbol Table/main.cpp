3676e8fe78c22b2e3f6ed4f09b51978ed93897cb
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-05-21 16:36:41 +0600

    Update main.cpp

--- 1. Symbol Table/main.cpp
+++ 1. Symbol Table/main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
 #include <string>
-#include "SymbolInfo.h"
+#include <vector>
+#include <fstream>
+#include "SymbolTable.h"
 
 using namespace std;
 
+vector<string> splitInput(string str){
+    vector<string> ans;
+    string delimiter = " ";
+    size_t pos = 0;
+    string token;
+    while ((pos = str.find(delimiter)) != string::npos) {
+        token = str.substr(0, pos);
+        ans.push_back(token);
+        str.erase(0, pos + delimiter.length());
+    }
+
+    return ans;
+}
+
 int main(){
-    cout << "hey";
+    string str;
+    ifstream input("input.txt");
+
+    if(input.is_open()){
+        while(input){
+            getline(input, str);
+
+            vector<string> res = splitInput(str);
+            for(int i = 0; i < res.size(); i++){
+                cout << res[i] << " ";
+            }
+            cout << endl;
+        }
+    }else cout << "Error opening file...";
+
     return 0;
 }
