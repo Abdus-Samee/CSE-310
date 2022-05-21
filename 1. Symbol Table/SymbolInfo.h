a6a669100c956e2373cf07c13fe169eeb887e8f4
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-05-21 15:45:45 +0600

    Create SymbolInfo.h

---
+++ 1. Symbol Table/SymbolInfo.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class SymbolInfo{
+        string name;
+        string type;
+    public:
+        SymbolInfo* next;
+        SymbolInfo();
+        SymbolInfo(string name, string type);
+        string getName(){ return this->name; }
+        string getType(){ return this->type; }
+        void setName(string name){ this->name = name; }
+        void setType(string type){ this->type = type; }
+};
+
+SymbolInfo::SymbolInfo(){}
+
+SymbolInfo::SymbolInfo(string name, string type){
+    this->name = name;
+    this->type = type;
+    next = NULL;
+}
