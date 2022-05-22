3b9dd52071683550261b4cf07393de1911c51733
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-05-22 23:24:12 +0600

    Update main.cpp

--- 1. Symbol Table/main.cpp
+++ 1. Symbol Table/main.cpp
@@ -25,37 +25,43 @@ int main(){
 
     if(input.is_open()){
         while(input){
-            getline(input, str);
+            if(getline(input, str)){
+                if(c == 0){
+                    int bucket_no = stoi(str);
+                    cout << bucket_no << endl;
+                    symbolTable = SymbolTable(bucket_no);
+                    c++;
+                }else{
+                    fstream outputFile;
+                    outputFile.open("output.txt", ios::out | ios::app);
+                    outputFile << str << endl;
+                    outputFile.close();
 
-            if(c == 0){
-                int bucket_no = stoi(str);
-                cout << bucket_no << endl;
-                symbolTable = SymbolTable(bucket_no);
-                c++;
-            }else{
-                vector<string> res = splitInput(str);
+                    vector<string> res = splitInput(str);
 
-                if(res[0] == "I"){
-                    string name = res[1];
-                    string type = res[2];
-                    symbolTable.insert(name, type);
-                }else if(res[0] == "L"){
-                    string name = res[1];
-                    SymbolInfo* info = symbolTable.lookup(name);
-                }else if(res[0] == "D"){
-                    string name = res[1];
-                    symbolTable.remove(name);
-                }else if(res[0] == "P"){
-                    if(res[1] == "A") symbolTable.printAllScopeTables();
-                    else if(res[1] == "C") symbolTable.printCurrentScopeTable();
-                    else cout << "Incorrect format of input" << endl;
-                }else if(res[0] == "S"){
-                    symbolTable.enterScope();
-                }else if(res[0] == "E"){
-                    symbolTable.exitScope();
-                }else cout << "Incorrect format of input..." << endl;
+                    if(res[0] == "I"){
+                        string name = res[1];
+                        string type = res[2];
+                        symbolTable.insert(name, type);
+                    }else if(res[0] == "L"){
+                        string name = res[1];
+                        SymbolInfo* info = symbolTable.lookup(name);
+                    }else if(res[0] == "D"){
+                        string name = res[1];
+                        symbolTable.remove(name);
+                    }else if(res[0] == "P"){
+                        if(res[1] == "A") symbolTable.printAllScopeTables();
+                        else if(res[1] == "C") symbolTable.printCurrentScopeTable();
+                        else cout << "Incorrect format of input" << endl;
+                    }else if(res[0] == "S"){
+                        symbolTable.enterScope();
+                    }else if(res[0] == "E"){
+                        symbolTable.exitScope();
+                    }else cout << "Incorrect format of input..." << endl;
+
+                    c++;
+                }
             }
-
         }
     }else cout << "Error opening file...";
 
