a26c660ae5ccc5e81882fd9a4d2d15066af4ef12
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-05-29 16:14:21 +0600

    Update and rename SymbolTable.h to 1805021_SymbolTable.h

--- 1. Symbol Table/1805021_SymbolTable.h
+++ 1. Symbol Table/1805021_SymbolTable.h
@@ -1,6 +1,6 @@
 #include <stack>
 #include <fstream>
-#include "ScopeTable.h"
+#include "1805021_ScopeTable.h"
 
 using namespace std;
 
@@ -37,21 +37,36 @@ void SymbolTable::enterScope(){
     cout << "New ScopeTable with id " << this->curr->getId() << " created" << endl;
 
     fstream outputFile;
-    outputFile.open("output.txt", ios::out | ios::app);
+    outputFile.open("1805021_output.txt", ios::out | ios::app);
     outputFile << "New ScopeTable with id " << this->curr->getId() << " created" << endl;
     outputFile.close();
 }
 
 void SymbolTable::exitScope(){
+    if(this->scopeStack.empty()){
+        cout << "NO CURRENT SCOPE" << endl;
+        fstream outputFile;
+        outputFile.open("1805021_output.txt", ios::out | ios::app);
+        outputFile << "NO CURRENT SCOPE" << endl;
+        outputFile.close();
+
+        return;
+    }
+
     ScopeTable* deletedTable = this->scopeStack.top();
     this->scopeStack.pop();
-    this->curr = this->scopeStack.top();
-    this->curr->setDeletedId(this->curr->getDeletedId() + 1);
+
+    if(!this->scopeStack.empty()){
+        this->curr = this->scopeStack.top();
+        this->curr->setDeletedId(this->curr->getDeletedId() + 1);
+    }else{
+        this->curr = NULL;
+    }
 
     cout << "ScopeTable with id " << deletedTable->getId() << " removed" << endl;
 
     fstream outputFile;
-    outputFile.open("output.txt", ios::out | ios::app);
+    outputFile.open("1805021_output.txt", ios::out | ios::app);
     outputFile << "ScopeTable with id " << deletedTable->getId() << " removed" << endl;
     outputFile.close();
 
@@ -59,14 +74,37 @@ void SymbolTable::exitScope(){
 }
 
 bool SymbolTable::insert(string name, string type){
+    if(this->scopeStack.empty()){
+        this->curr = new ScopeTable(this->total_buckets);
+        this->scopeStack.push(this->curr);
+    }
     return this->curr->insert(name, type);
 }
 
 bool SymbolTable::remove(string name){
+    if(this->scopeStack.empty()){
+        cout << "NO SCOPE TO REMOVE FROM" << endl;
+        fstream outputFile;
+        outputFile.open("1805021_output.txt", ios::out | ios::app);
+        outputFile << "NO SCOPE TO REMOVE FROM" << endl;
+        outputFile.close();
+
+        return false;
+    }
     return this->curr->deleteEntry(name);
 }
 
 SymbolInfo* SymbolTable::lookup(string name){
+    if(this->scopeStack.empty()){
+        cout << "NO SCOPE TO LOOKUP FROM" << endl;
+        fstream outputFile;
+        outputFile.open("1805021_output.txt", ios::out | ios::app);
+        outputFile << "NO SCOPE TO LOOKUP FROM" << endl;
+        outputFile.close();
+
+        return NULL;
+    }
+
     SymbolInfo* res = NULL;
     while(true){
         SymbolInfo* temp = this->curr->lookup(name);
@@ -85,7 +123,7 @@ SymbolInfo* SymbolTable::lookup(string name){
         cout << "Not found\n";
 
         fstream outputFile;
-        outputFile.open("output.txt", ios::out | ios::app);
+        outputFile.open("1805021_output.txt", ios::out | ios::app);
         outputFile << "Not found" << endl;
         outputFile.close();
     }
@@ -94,10 +132,30 @@ SymbolInfo* SymbolTable::lookup(string name){
 }
 
 void SymbolTable::printCurrentScopeTable(){
+    if(this->scopeStack.empty()){
+        cout << "NO CURRENT SCOPE" << endl;
+        fstream outputFile;
+        outputFile.open("1805021_output.txt", ios::out | ios::app);
+        outputFile << "NO CURRENT SCOPE" << endl;
+        outputFile.close();
+
+        return;
+    }
+
     this->curr->print();
 }
 
 void SymbolTable::printAllScopeTables(){
+    if(this->scopeStack.empty()){
+        cout << "NO SCOPE AVAILABLE" << endl;
+        fstream outputFile;
+        outputFile.open("1805021_output.txt", ios::out | ios::app);
+        outputFile << "NO SCOPE AVAILABLE" << endl;
+        outputFile.close();
+
+        return;
+    }
+
     stack<ScopeTable*> tempStack = this->scopeStack;
     while(!tempStack.empty()){
         tempStack.top()->print();
