8f1055f207a2597c79f2945690379fe3e2179f63
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2022-06-18 02:13:25 +0600

    Update 1805021_SymbolInfo.h

--- 3. Syntax & Semantic Analyzer/1805021_SymbolInfo.h
+++ 3. Syntax & Semantic Analyzer/1805021_SymbolInfo.h
@@ -8,25 +8,23 @@ class SymbolInfo{
         string type;
     public:
         SymbolInfo* next;
-        SymbolInfo();
-        SymbolInfo(string name, string type);
-        ~SymbolInfo();
+
+        SymbolInfo(){
+            next = NULL;
+        }
+
+        SymbolInfo(string name, string type){
+            this->name = name;
+            this->type = type;
+            next = NULL;
+        }
+
+        ~SymbolInfo(){
+            next = NULL;
+        }
+        
         string getName(){ return this->name; }
         string getType(){ return this->type; }
         void setName(string name){ this->name = name; }
         void setType(string type){ this->type = type; }
 };
-
-SymbolInfo::SymbolInfo(){
-    next = NULL;
-}
-
-SymbolInfo::SymbolInfo(string name, string type){
-    this->name = name;
-    this->type = type;
-    next = NULL;
-}
-
-SymbolInfo::~SymbolInfo(){
-    next = NULL;
-}
